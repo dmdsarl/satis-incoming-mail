f3f072d89b729803dcddc9589a21db48589920fd
Patrick AMOUSSOU <amoussou@dmdconsult.com>
2021-07-26 09:47:11 +0100

    update incoming mail comment

--- src/config/incoming_mail.php
+++ src/config/incoming_mail.php
@@ -1,26 +1,26 @@
 <?php
 return [
-    "table" => "my_table", // Représente le nom de la table dans laquelle vous voulez enregistrer les différents mails reçus
-    "message_format" => "html_text", //  Le format d'enregistrement des messages reçu depuis le mail :
-    // html_text => pour enregistrer le message en format html et plain_text pour le plain text
-    "object" => [ // Les différents champs qui seront prise en compte pour enregistrer dans la table
-        'first_name' => 'first_name', // Personnaliser le nom du champs du prénom de l"expéditeur
-        'last_name' => 'last_name', // Personnaliser le nom du champs du nom de l"expéditeur
-        'email' => 'email', // Personnaliser le nom du champs du mail
-        "message"=>"message", // Personnaliser le nom du champs du message a enregistré
-        "date"=>"date", // Personnaliser le nom du champs du date d'envoie du mail
-        "attachments"=>"attachments", // Personnaliser le nom du champs des fichiers, ce champs est de type json
+    "table" => "my_table", // Represents the name of the table in which you want to save the different received mails
+    "message_format" => "html_text", // The format in which you want to save the messages received from the mail :
+    // html_text => to save the message in html format and plain_text for plain text
+    "object" => [ // The different fields that will be taken into account to record in the table
+        "first_name" => "first_name", // Customize the name of the first name field of the sender
+        "last_name" => "last_name", // Customize the name of the sender "s name field
+        "email" => "email", // Customize the name of the email field
+        "message" => "message", // Customize the field name of the message to be saved
+        "date" => "date", // Customize the field name of the date the mail was sent
+        "attachments" => "attachments", // Customize the name of the file field, this field is of type json
     ],
-    "app_registration"=>[ //Informations de subscription au service de incoming mail
-        'app_name' => 'Care - Rwanda', // Nom de votre application,
-        'url' => 'http://localhost:8000/satis/incoming/mail', // Lien d'enregistrement des mails reçus
-        'mail_server' => 'smtp.gmail.com', // Maim host
-        'mail_server_username' => 'test.dmd.arafath@gmail.com', // Mail username
-        'mail_server_password' => 'P@ssword@DMD', // Mail password
-        'mail_server_port' => '587', // Mail port
-        'app_login_url' => 'http://localhost:8000/oauth/token', // Authentification api
-        'app_login_params' => [ // Informations pour se connecter à votre projet. Les différents colunms à changer suivant
-            // les arguments nécessaires pour se connecter à votre project
+    "app_registration" => [ //Incoming mail service subscription information
+        "app_name" => 'Care - Rwanda', // Name of your application,
+        "url" => 'http://localhost:8000/satis/incoming/mail', // Registration link for incoming mail
+        "mail_server" => 'smtp.gmail.com', // Maim host
+        "mail_server_username" => 'test.dmd.arafath@gmail.com', // Mail username
+        "mail_server_password" => 'P@ssword@DMD', // Mail password
+        "mail_server_port" => '587', // Mail port
+        "app_login_url" => 'http://localhost:8000/oauth/token', // Api authentication
+        "app_login_params" => [ // Information to connect to your project. The different colunms to change following
+            // arguments needed to connect to your project
             "grant_type" => "password",
             "client_id" => "93f64252-da39-4f90-86e8-c14a500c6d39",
             "client_secret" => "r3CcD12cCIhpMJAwI7ipcfJw8eab01Mprq81f7vc",
